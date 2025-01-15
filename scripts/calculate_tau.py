72a41bdb566a96fc8734a7924d3a34353d88a050
Irina Pochinok <irina.pochinok@zmnh.uni-hamburg.de>
2025-01-15 13:37:47 +0100

    fit tau, added log_message to return vals

--- scripts/calculate_tau.py
+++ scripts/calculate_tau.py
@@ -25,14 +25,11 @@ def fit_single_exp(ydata_to_fit_, start_idx_=1):
     """
     Fit function func_exp to data using non-linear least square.
 
-    todo check that - important point: Fit is done from the first ACF value (acf[0] is skipped, it is done like this
-    in the papers, still not sure)
-
     :param ydata_to_fit_: 1d array, the dependant data to fit
     :param start_idx_: int, index to start fitting from
-    :return: fit_popt, fit_pcov, tau, fit_r_squared
+    :return: fit_popt, fit_pcov, tau, fit_r_squared, log_message
     """
-    t = np.linspace(0, len(ydata_to_fit_)-1, len(ydata_to_fit_)).astype(int)
+    t = np.linspace(0, len(ydata_to_fit_) - 1, len(ydata_to_fit_)).astype(int)
 
     with warnings.catch_warnings():
         warnings.filterwarnings('error')
@@ -45,18 +42,23 @@ def fit_single_exp(ydata_to_fit_, start_idx_=1):
             # fit r-squared
             y_pred = func_single_exp(t[start_idx_:], *popt)
             fit_r_squared = r2_score(ydata_to_fit_[start_idx_:], y_pred)
+            log_message = 'ok'
         except RuntimeError as e:
-            print('RuntimeError: {}'. format(e))
+            print('RuntimeError: {}'.format(e))
             fit_popt, fit_pcov, tau, fit_r_squared = np.nan, np.nan, np.nan, np.nan
+            log_message = 'RuntimeError'
         except OptimizeWarning as o:
-            print('OptimizeWarning: {}'. format(o))
+            print('OptimizeWarning: {}'.format(o))
             fit_popt, fit_pcov, tau, fit_r_squared = np.nan, np.nan, np.nan, np.nan
+            log_message = 'OptimizeWarning'
         except RuntimeWarning as re:
-            print('RuntimeWarning: {}'. format(re))
+            print('RuntimeWarning: {}'.format(re))
             fit_popt, fit_pcov, tau, fit_r_squared = np.nan, np.nan, np.nan, np.nan
+            log_message = 'RuntimeWarning'
         except ValueError as ve:
-            print('ValueError: {}'. format(ve))
+            print('ValueError: {}'.format(ve))
             print('Possible reason: acf contains NaNs, low spike count')
             fit_popt, fit_pcov, tau, fit_r_squared = np.nan, np.nan, np.nan, np.nan
+            log_message = 'ValueError'
 
-    return fit_popt, fit_pcov, tau, fit_r_squared
+    return fit_popt, fit_pcov, tau, fit_r_squared, log_message
