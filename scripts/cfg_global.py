f1a9eae5080b77ad62128843bc469ed283bedf4d
iinnpp <i.pochinok@gmail.com>
2025-03-19 07:46:04 +0100

    sync with home

--- scripts/cfg_global.py
+++ scripts/cfg_global.py
@@ -1,5 +1,5 @@
 # global configuration
 
 # root folder for the project
-project_folder_path = 'Q:\\Personal\\Irina\\projects\\isttc\\'
-
+# project_folder_path = 'Q:\\Personal\\Irina\\projects\\isttc\\'
+project_folder_path = 'D:\\work\\q_backup_06_03_2025\\projects\\isttc\\'
