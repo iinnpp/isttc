aef27cef01b75e199082bc65c7b6f39e26663e98
iinnpp <i.pochinok@gmail.com>
2025-04-02 14:56:36 +0200

    plots for suppl, resampling procedure

--- scripts/estimate_resampling_params.py
+++ scripts/estimate_resampling_params.py
@@ -11,6 +11,27 @@ from scripts.calculate_tau import fit_single_exp, func_single_exp_monkey
 from scripts.spike_train_utils import get_trials, bin_trials
 from scripts.cfg_global import project_folder_path
 
+
+def sample_signals(units_df, min_per_area, n_total):
+    sampled_units = []
+
+    area_counts = units_df["ecephys_structure_acronym"].value_counts()
+    base_samples = {area: min(min_per_area, count) for area, count in area_counts.items()}
+    remaining_samples = n_total - sum(base_samples.values())
+    total_remaining = sum(area_counts) - sum(base_samples.values())
+    print(
+        f'total {n_total}, min per area {min_per_area}. Base samples {base_samples}, \nproportional samples {remaining_samples}')
+
+    # get min_per_area, then sample proportionally
+    for area, count in area_counts.items():
+        base_samples = min(min_per_area, count)
+        extra_samples = int((count / total_remaining) * remaining_samples) if total_remaining > 0 else 0
+        n_samples = base_samples + extra_samples
+        sampled_units.extend(units_info_df[units_info_df["ecephys_structure_acronym"] == area]
+                             .sample(n=min(n_samples, count), random_state=42)["unit_id"].tolist())
+
+    return sampled_units
+
 if __name__ == "__main__":
     dataset_folder = project_folder_path + 'results\\allen_mice\\dataset\\cut_30min\\'
 
@@ -20,6 +41,7 @@ if __name__ == "__main__":
         reader = csv.reader(f)
         sua_list = list(reader)
     print(f'Loaded N units {len(sua_list)}')
+    units_info_df = pd.read_pickle(dataset_folder + 'sua_list_constrained_units_df.pkl')
 
     # estimate M
     fs = 30000
@@ -29,15 +51,21 @@ if __name__ == "__main__":
     trial_len = int(n_lags * bin_size * (fs / 1000))
 
     n_trials = 40  # this is fixed based on experimental datasets
-    m_iterations = [20, 40, 60, 80, 100, 150, 200, 500, 1000]
+    #m_iterations = [20, 40, 60, 80, 100, 150, 200, 500, 1000]
+    m_iterations = [50, 100, 200, 500, 1000]
 
-    n_signals = [20, 50, 100]
+    n_total_signal_l = [100]
+    min_signal_per_area_l = [10]
 
-    for n_signal in n_signals:
-        print(f'Running for n_signal = {n_signal}')
-        random_signals = random.sample(sua_list, n_signal)
+    for n_total_signals, min_signal_per_area in zip(n_total_signal_l, min_signal_per_area_l):
+        print(f'Running for n_total_signals = {n_total_signals}, min_signal_per_area = {min_signal_per_area}')
+        units_to_sample = sample_signals(units_info_df, min_signal_per_area, n_total_signals)
+        #print(units_to_sample)
+        random_signals = [item for item in sua_list if item[2] in units_to_sample]
+        print(len(random_signals))
+        #random_signals = random.sample(sua_list, n_signal)
 
-        output_log = dataset_folder + f'resampling//resampling_params_estimate_{n_signal}_signals.txt'
+        output_log = dataset_folder + f'resampling//resampling_params_estimate_{n_total_signals}_signals.txt'
         old_stdout = sys.stdout
         sys.stdout = open(output_log, 'w')
 
@@ -60,9 +88,9 @@ if __name__ == "__main__":
                     tau_l.append(tau)
                 tau_dict[m_iteration] = tau_l
 
-            signal_tau_dict[signal_idx] = tau_dict
+            signal_tau_dict[signal[2]] = tau_dict
 
-        with open(dataset_folder + f'resampling//signal_tau_dict_{n_signal}_signals.pkl', "wb") as f:
+        with open(dataset_folder + f'resampling//signal_tau_dict_{n_total_signals}_signals.pkl', "wb") as f:
             pickle.dump(signal_tau_dict, f)
 
         sys.stdout = old_stdout
